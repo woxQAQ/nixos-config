b956c09c8402708ff50114566942d3e2a6c31407
woxQAQ <woxqaq@gmail.com>
2025-03-22 23:27:46 +0800

    chore: wsl git config

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,15 +1,19 @@
-{pkgs,stateVersion, ...}: {
+{pkgs,stateVersion,username, ...}: {
   networking.hostName = "wsl"; # Define your hostname.
   system.stateVersion = stateVersion;
   wsl = {
     enable = true;
-    defaultUser="woxQAQ";
+    defaultUser=username;
     startMenuLaunchers = true;
   };
   nixpkgs = {
     config = {
       allowUnfree = true;
     };
+  };
+  programs.git.config = {
+    user.name=username;
+    user.gmail="woxqaq@gmail.com";
   };
   environment.systemPackages = with pkgs;[
     go_1_23
