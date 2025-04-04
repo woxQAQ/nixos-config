b2c68d7c489392123c6fae8a3cfa43561325a0d8
woxQAQ <woxqaq@gmail.com>
2025-04-04 13:03:51 +0800

    chore: update

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-gaming.cachix.org"
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://cache.nixos.org"
+      "https://anyrun.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
     ];
   };
 
--- modules/base.nix
+++ modules/base.nix
@@ -62,23 +62,15 @@
 
   environment.variables.EDITOR = "vim";
   nix.settings.substituters = [
-    "https://nix-gaming.cachix.org"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://cache.nixos.org"
-  ];
-  nix.settings.extra-substituters = [
+    "https://mirrors.sustech.edu.cn/nix-channels/store"
     "https://nix-community.cachix.org"
-    # "https://hyprland.cachix.org"
-    "https://anyrun.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
-    "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-  ];
-  nix.settings.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-    # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
   ];
+  builders-use-substitutes = true;
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
