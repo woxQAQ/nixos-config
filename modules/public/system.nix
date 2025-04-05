a30ec35e818c1b48f5f9ebe2c9fcecafa7714396
woxQAQ <woxqaq@gmail.com>
2025-04-05 17:05:00 +0800

    chore: update

--- modules/public/system.nix
+++ modules/public/system.nix
@@ -1,24 +1,26 @@
 {lib, ...}: {
-  nix.settings = {
+  nix = {
     gc = {
       automatic = lib.mkDefault true;
       dates = lib.mkDefault "weekly";
       options = lib.mkDefault "--delete-older-than 7d";
     };
-    auto-optimise-store = true;
-    substituters = [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://mirrors.sustech.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
-    ];
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    builders-use-substitutes = true;
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-    ];
+    settings = {
+      auto-optimise-store = true;
+      substituters = [
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://mirrors.sustech.edu.cn/nix-channels/store"
+        "https://nix-community.cachix.org"
+      ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      builders-use-substitutes = true;
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      ];
+    };
   };
 }
