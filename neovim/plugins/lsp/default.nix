273bb4bdb2e7d13a05244ad07169f905131ecd7d
woxQAQ <woxqaq@gmail.com>
2025-05-05 18:03:59 +0800

    fix: lsp keymaps

--- neovim/plugins/lsp/default.nix
+++ neovim/plugins/lsp/default.nix
@@ -7,8 +7,7 @@
   data = scanPlugins ./. args;
 in
   lib.attrsets.mergeAttrsList [
-    (lib.attrsets.mergeAttrsList
-      data)
+    (lib.attrsets.mergeAttrsList data)
     {
       lsp-format = {
         enable = true;
@@ -30,6 +29,60 @@ in
       lsp = {
         enable = true;
         inlayHints = true;
+        keymaps = {
+          silent = true;
+          # diagnostic = {
+          #   # Navigate in diagnostics
+          #   "]e" = "goto_prev";
+          #   "[e" = "goto_next";
+          # };
+
+          lspBuf = {
+            # gd = "definition";
+            gD = "references";
+            # gt = "type_definition";
+            # K = "hover";
+            # "<F2>" = "rename";
+          };
+
+          extra = [
+            {
+              action = "<CMD>Lspsaga code_action<CR>";
+              key = "<leader>ca";
+              options.desc = "code action";
+            }
+            {
+              action = "<CMD>Lspsaga rename<CR>";
+              key = "<leader>cr";
+              options.desc = "rename";
+            }
+            {
+              action = "<CMD>Lspsaga finder<CR>";
+              key = "gr";
+              options.desc = "find references";
+            }
+            {
+              action = "<CMD>Lspsaga finder imp<CR>";
+              key = "gi";
+              options.desc = "find implement";
+            }
+            {
+              action = "<CMD>Lspsaga peek_definition<CR>";
+              key = "gd";
+              options.desc = "find definition";
+            }
+            {
+              action = "<CMD>Lspsaga peek_type_definition<CR>";
+              key = "gt";
+              options.desc = "find type definition";
+            }
+            {
+              action = "<CMD>Lspsaga hover_doc<CR>";
+              key = "K";
+              options.desc = "hover doc";
+            }
+          ];
+        };
         servers = {
           clangd.enable = true;
           nil_ls = {
