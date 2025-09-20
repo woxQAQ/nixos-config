e4259fcdae6833dbd02132eb79e70add206bccb9
woxQAQ <woxqaq@gmail.com>
2025-09-20 22:10:37 +0800

    fix: editor conflict

--- modules/public/programs.nix
+++ modules/public/programs.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  environment.variables.EDITOR = "nvim --clean";
+  # environment.variables.EDITOR = "nvim --clean";
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 
