f64cb20fe056fe774c5eba193e11b26bb2826994
woxQAQ <woxqaq@gmail.com>
2025-09-21 00:58:48 +0800

    chore[check]: disbale starship catppuccin for darwin

--- home/public/catppuccin.nix
+++ home/public/catppuccin.nix
@@ -1,4 +1,8 @@
-{ catppuccin, ... }:
+{
+  catppuccin,
+  pkgs,
+  ...
+}:
 {
   imports = [
     catppuccin.homeModules.catppuccin
@@ -7,6 +11,7 @@
     enable = true;
     flavor = "mocha";
     accent = "pink";
+    starship.enable = pkgs.stdenv.isLinux;
     zed = {
       enable = false;
       flavor = "frappe";
