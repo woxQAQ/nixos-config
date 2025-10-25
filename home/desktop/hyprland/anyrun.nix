b3a20e29e6b824298a5542ef3e3b855aaf942d34
woxQAQ <woxqaq@gmail.com>
2025-10-26 00:56:27 +0800

    refactor: anyrun

--- home/desktop/hyprland/anyrun.nix
+++ home/desktop/hyprland/anyrun.nix
@@ -26,73 +26,20 @@
       ];
 
       width.fraction = 0.3;
-      y.absolute = 15;
-      hidePluginInfo = true;
+      x.fraction = 0.5;
+      y.fraction = 0.05;
+      hideIcons = false;
+      ignoreExclusiveZones = false;
+      layer = "overlay";
+      hidePluginInfo = false;
       closeOnClick = true;
+      showResultsImmediately = true;
+      maxEntries = null;
     };
-    extraCss =
-      let
-        font-size = "1.3rem";
-        # font-family = "noto sans, FontAwesome";
-        font-family = "Maple Mono NF CN";
-      in
-      ''
-        @define-color bg-col  rgba(30, 30, 46, 0.7);
-        @define-color bg-col-light rgba(150, 220, 235, 0.7);
-        @define-color border-col rgba(30, 30, 46, 0.7);
-        @define-color selected-col rgba(150, 205, 251, 0.7);
-        @define-color fg-col #D9E0EE;
-        @define-color fg-col2 #F28FAD;
-
-        * {
-          transition: 200ms ease;
-          font-family: ${font-family};
-          font-size: ${font-size};
-        }
-
-        #window {
-          background: transparent;
-        }
-
-        #plugin,
-        #main {
-          border: 3px solid @border-col;
-          color: @fg-col;
-          background-color: @bg-col;
-        }
-        /* anyrun's input window - Text */
-        #entry {
-          color: @fg-col;
-          background-color: @bg-col;
-        }
-
-        /* anyrun's ouput matches entries - Base */
-        #match {
-          color: @fg-col;
-          background: @bg-col;
-        }
-
-        /* anyrun's selected entry - Red */
-        #match:selected {
-          color: @fg-col2;
-          background: @selected-col;
-        }
-
-        #match {
-          padding: 3px;
-          border-radius: 16px;
-        }
-
-        #entry, #plugin:hover {
-          border-radius: 16px;
-        }
-
-        box#main {
-          background: @bg-col;
-          border: 1px solid @border-col;
-          border-radius: 15px;
-          padding: 5px;
-        }
-      '';
+    extraConfigFiles = {
+      "symbols.ron".source = ./conf/anyrun/symbols.ron;
+      "applications.ron".source = ./conf/anyrun/app.ron;
+    };
   };
+  xdg.configFile."anyrun/style.css".source = ./conf/anyrun/style.css;
 }
