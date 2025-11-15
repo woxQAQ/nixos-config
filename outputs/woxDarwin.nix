028feb4e3b1f30e7af675baf2574f4aea1917abf
woxQAQ <woxqaq@gmail.com>
2025-11-16 01:29:46 +0800

    neovim as module

--- outputs/woxDarwin.nix
+++ outputs/woxDarwin.nix
@@ -21,6 +21,7 @@ let
       modules.public = {
         cloud-native.enable = true;
         desktop.enable = true;
+        terminal.neovim.enable = true;
       };
     }
   ];
--- outputs/woxQAQ.nix
+++ outputs/woxQAQ.nix
@@ -29,6 +29,7 @@ let
       modules.public = {
         cloud-native.enable = true;
         desktop.enable = true;
+        terminal.neovim.enable = true;
       };
     }
   ]
