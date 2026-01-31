af3b5c2c6641868bcd4c9b87ec032f373cd80dcb
woxQAQ <woxqaq@gmail.com>
2026-01-31 21:24:58 +0800

    feat: rime ice as rime data

--- home/desktop/fcitx/default.nix
+++ home/desktop/fcitx/default.nix
@@ -14,12 +14,29 @@ in
       source = ./profile.conf;
       force = true;
     };
+
+    # Rime configuration files
+    home.file.".local/share/fcitx5/rime/default.custom.yaml".text =
+      #yaml
+      ''
+        patch:
+          __include: rime_ice_suggestion:/
+          schema_list: 
+            - schema: rime_ice
+      '';
+
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
       fcitx5.waylandFrontend = true;
       fcitx5.addons = with pkgs; [
-        fcitx5-rime
+        (fcitx5-rime.override {
+          rimeDataPkgs = [
+            rime-ice
+          ];
+        })
+        # rime-ice
+        fcitx5-fluent
         qt6Packages.fcitx5-configtool
         fcitx5-gtk
       ];
