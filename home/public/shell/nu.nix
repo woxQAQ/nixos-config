51a4a3031d6d2bbc5b2f815fe925f7b4cd285768
woxQAQ <woxqaq@gmail.com>
2026-02-13 05:11:24 +0800

    chore: use glm newest model

--- home/public/shell/nu.nix
+++ home/public/shell/nu.nix
@@ -25,7 +25,7 @@
           glm: {
             api_key: $env.ZAI_API_KEY_ALT,
             base_url: "https://open.bigmodel.cn/api/anthropic",
-            model: null
+            model: "glm-x-preview"
           },
           volcengine: {
             api_key: $env.ARK_CODE_API_KEY,
