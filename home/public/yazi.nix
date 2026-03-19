4d69339f2a478e345852b45428f46278869c4441
woxQAQ <woxqaq@gmail.com>
2026-03-20 05:43:37 +0800

    feat: yazi git plugin

--- home/public/yazi.nix
+++ home/public/yazi.nix
@@ -6,10 +6,34 @@
     package = pkgs.yazi;
     enableBashIntegration = true;
     enableNushellIntegration = true;
+    plugins = {
+      inherit (pkgs.yaziPlugins) git;
+    };
+    initLua = # lua
+      ''
+        require("git"):setup {
+          -- Order of status signs showing in the linemode
+          order = 1500,
+        }
+      '';
     settings = {
       mgr = {
         show_hidden = true;
         sort_dir_first = true;
+      };
+      plugin = {
+        prepend_fetchers = [
+          {
+            id = "git";
+            url = "*";
+            run = "git";
+          }
+          {
+            id = "git";
+            url = "*/";
+            run = "git";
+          }
+        ];
       };
     };
   };
