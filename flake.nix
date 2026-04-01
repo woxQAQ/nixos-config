8bc0d353a6fdfbc1ec38ffdbd8ed4be14f3458b8
woxQAQ <woxqaq@gmail.com>
2026-04-01 20:27:23 +0800

    flake: bump

--- flake.nix
+++ flake.nix
@@ -1,14 +1,13 @@
 {
   description = "woxQAQ's NixOS flake";
   #
-  # nixConfig = {
-  #   extra-substituters = [
-  #     "https://nix-community.cachix.org"
-  #   ];
-  #   extra-trusted-public-keys = [
-  #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  #   ];
-  # };
+  nixConfig = {
+    extra-substituters = [
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+    ];
+    extra-trusted-public-keys = [
+    ];
+  };
 
   outputs = inputs: import ./outputs inputs;
   inputs = {
