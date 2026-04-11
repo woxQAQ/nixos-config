21c9aed0231427b9580f5d604eee0c94a65b44b9
woxQAQ <woxqaq@gmail.com>
2026-04-11 18:05:28 +0800

    chore: update

--- hosts/selfcloud/storage.nix
+++ hosts/selfcloud/storage.nix
@@ -21,9 +21,10 @@
   systemd.tmpfiles.rules = [
     # 类型 路径 模式 用户 组 清理策略
     "d /mnt/data 0755 ${username} users -"
-    "d /mnt/data/1000 0755 ${username} users -"
-    "d /mnt/data/1000/videos 0755 ${username} users -"
-    "d /mnt/data/1000/data 0755 ${username} users -"
+    # 修复 ACL：子目录 group 权限只有 --x，需要改为 r-x 才能让 users 组读取
+    "a /mnt/data/1000 - - - - group::r-x default:group::r-x"
+    "a /mnt/data/1000/videos - - - - group::r-x default:group::r-x"
+    "a /mnt/data/1000/data - - - - group::r-x default:group::r-x"
   ];
   fileSystems."/mnt/data" = {
     device = "/dev/disk/by-uuid/4617589b-04c3-4534-8c4a-9c4667116b6c";
