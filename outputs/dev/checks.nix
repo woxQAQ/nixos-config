c596718655cd1054c51ec98ff958d78ba04683a7
woxQAQ <woxqaq@gmail.com>
2026-04-14 17:08:03 +0800

    chore: update pre-commit checks option

--- outputs/dev/checks.nix
+++ outputs/dev/checks.nix
@@ -1,25 +1,39 @@
-{ inputs, ... }:
+{
+  inputs,
+  lib,
+  self,
+  pkgs,
+  ...
+}:
 {
   imports = [
     inputs.git-hooks.flakeModule
   ];
   perSystem = {
     pre-commit = {
-      check.enable = true;
+      check.enable = false;
       settings.excludes = [ "flake.lock" ];
 
       settings.hooks = {
         # keep-sorted start block=yes
         actionlint.enable = true;
         deadnix = {
-          enable = true;
+          enable = false;
           settings = {
             edit = true;
           };
         };
         statix.enable = true;
+        treefmt.enable = true;
         # keep-sorted end
       };
     };
+
+    checks = lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin (
+      lib.mapAttrs' (name: cfg: {
+        name = "darwin-${name}";
+        value = cfg.system;
+      }) self.darwinConfigurations
+    );
   };
 }
